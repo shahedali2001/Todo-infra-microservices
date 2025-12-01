7f11530113ccd3b3c2c0242057e8f28474cc857a
HASEEBA <haseebafaiza23@gmail.com>
2025-12-01 11:09:35 +0530

    upated ipconfig

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     storage_account_name = "shahedstg881"
     container_name       = "tfscontainer"
     key                  = "dev.terraform.tfstate"
-    resource_group_name  = "shahed-dev-01"
+    resource_group_name  = "shahed-rg-01"
     subscription_id      = "2c6da294-c904-4801-bffd-cd7f0b236e3d"
     use_azuread_auth     = true
   }
